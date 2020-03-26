ce88364bf5ca4bf8d1afa5f4a1bd5334163c4d15
Katerina Michalickova <kmichali@imperial.ac.uk>
2020-03-26 20:23:36 +0000

    add print statement

--- velocity.py
+++ velocity.py
@@ -1,4 +1,5 @@
 # this code sets velocity
+
 velocity = 10.0
 for i in range(5): # execute the loop 5 times
     print(i, ':', velocity)
@@ -8,6 +9,8 @@ for i in range(5): # execute the loop 5 times
     else:
         print('moving too slow')
         velocity = velocity + 10.0
+
+#print final velocity
 print('final velocity:', velocity)
 
 
