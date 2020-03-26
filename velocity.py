4175a75c449515189e70eaaf698d63fd35399f39
Katerina Michalickova <kmichali@imperial.ac.uk>
2020-03-26 19:46:37 +0000

    initial velocity code

---
+++ velocity.py
@@ -0,0 +1,12 @@
+velocity = 10.0
+for i in range(5): # execute the loop 5 times
+    print(i, ':', velocity)
+    if velocity > 20.0:
+        print('moving too fast')
+        velocity = velocity - 5.0
+    else:
+        print('moving too slow')
+        velocity = velocity + 10.0
+print('final velocity:', velocity)
+
+
